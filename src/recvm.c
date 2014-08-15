f1585845585a16ba6a6873714586b62da3f80562
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-15 14:44:29 +0200

    Stub project structure.

---
+++ src/recvm.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s interval", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+}
