2dcfa761fccac9ad00d1051dc3bb84033407ce82
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-15 10:41:07 +0200

    Stub solution to third exercise in second assignment.

--- ex/02/leggistud.c
+++ ex/02/leggistud.c
@@ -1,12 +1,31 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct student {
+	char* name;
+	char* surname;
+	char* id;
+} student;
 
 int main (int argc, char *argv[])
 {
 	if (argc != 2) {
-		printf("Usage: %s nome_file\n", argv[0]);
+		fprintf(stderr, "Usage: %s nome_file\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	return 0;
+	FILE *input = fopen(argv[1], "r");
+	if (errno == ENOENT) {
+		perror(argv[1]);
+		return EXIT_FAILURE;
+	}
+
+	char name[256], surname[256], id[8];
+	while (fscanf(input, "%[^:]:%[^:]:%[^:\n]\n", name, surname, id) != EOF) {
+	}
+
+	fclose(input);
+
+	return EXIT_SUCCESS;
 }
