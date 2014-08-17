29bd8c1a6c487a793f4fb6810c1454bfc1283a8f
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-18 00:04:01 +0200

    Use macros instead of globals.

--- src/constants.h
+++ src/constants.h
@@ -1,11 +1,11 @@
 #ifndef _constants_h
 #define _constants_h
 
-#define TRUE 1
+#define TRUE  1
 #define FALSE 0
 
-int BETWEEN_SYMBOLS = 1;
-int BETWEEN_LETTERS = 3;
-int BETWEEN_WORDS = 6;
+#define BETWEEN_SYMBOLS 1
+#define BETWEEN_LETTERS 3
+#define BETWEEN_WORDS   6
 
 #endif
