a5aae91b336cdd06133404df559dc225ce9cc9cf
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-17 20:44:38 +0200

    Extract method send_symbol.

--- src/morse_signal.c
+++ src/morse_signal.c
@@ -7,6 +7,14 @@
 #include "morse_signal.h"
 #include "msleep.h"
 
+void send_symbol (char c, pid_t pid)
+{
+	if (c == '.')
+		kill(pid, SIGUSR1);
+	else if (c == '-')
+		kill(pid, SIGUSR2);
+}
+
 void send_letter (char c, pid_t pid, int interval)
 {
 	int i; const char *s = char_to_morse(c);
@@ -14,16 +22,12 @@ void send_letter (char c, pid_t pid, int interval)
 	if (s == NULL)
 		return;
 
-	for (i = 0; i < strlen(s); i++) {
-		if (s[i] == '.') {
-			kill(pid, SIGUSR1);
-		} else if (s[i] == '-') {
-			kill(pid, SIGUSR2);
-		}
-
+	for (i = 0; i < strlen(s) - 1; i++) {
+		send_symbol(s[i], pid);
 		msleep(interval * BETWEEN_SYMBOLS);
 	}
 
+	send_symbol(s[i], pid);
 	msleep(interval * BETWEEN_LETTERS);
 }
 
--- src/morse_signal.h
+++ src/morse_signal.h
@@ -1,6 +1,7 @@
 #ifndef _morse_signal_h
 #define _morse_signal_h
 
+void send_symbol (char, pid_t);
 void send_letter (char, pid_t, int);
 void end_word (int);
 
