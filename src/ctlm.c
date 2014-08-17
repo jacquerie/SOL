b7bf1a5ebd8e0fcff5a96cfc895525467f6f9691
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-17 11:42:06 +0200

    Remove most of ctml.c content.

--- src/ctlm.c
+++ src/ctlm.c
@@ -3,23 +3,10 @@
 
 int main (int argc, char *argv[])
 {
-	int i, interval, last_interval, length; no_errors, test_cases, test_size;
-
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
 		return EXIT_FAILURE;
 	}
-
-	interval = atoi(ARGV[1]);
-	test_cases = atoi(ARGV[2]);
-
-	no_errors = 1;
-	while (no_errors) {
-		for (i = 0; i < test_cases; i++) {
-		}
-	}
-
-	no_errors
 
 	return EXIT_SUCCESS;
 }
