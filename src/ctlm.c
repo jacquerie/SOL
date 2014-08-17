8d77de91880077e0f0265b70281db60f41684740
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-17 06:37:56 +0200

    Add basic structure of ctlm.

--- src/ctlm.c
+++ src/ctlm.c
@@ -3,10 +3,23 @@
 
 int main (int argc, char *argv[])
 {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s interval\n", argv[0]);
+	int i, interval, last_interval, length; no_errors, test_cases, test_size;
+
+	if (argc != 3) {
+		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+
+	interval = atoi(ARGV[1]);
+	test_cases = atoi(ARGV[2]);
+
+	no_errors = 1;
+	while (no_errors) {
+		for (i = 0; i < test_cases; i++) {
+		}
+	}
+
+	no_errors
 
 	return EXIT_SUCCESS;
 }
