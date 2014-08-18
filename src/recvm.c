745a057e9264948052287da2e215fd28ff4070ea
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-18 06:11:07 +0200

    Completely overhaule structure.

--- src/recvm.c
+++ src/recvm.c
@@ -1,67 +1,15 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
-#include <unistd.h>
 
 #include "constants.h"
-
-volatile sig_atomic_t keep_going = TRUE;
-
-static int interval;
-static struct timeval last_signal;
-
-void quit (int sig)
-{
-	keep_going = FALSE;
-}
-
-void receive_dot (int sig)
-{
-	struct timeval this_signal;
-	gettimeofday(&this_signal, NULL);
-
-	int difference = (this_signal.tv_sec * 1000 + this_signal.tv_usec / 1000) -
-		(last_signal.tv_sec * 1000 + last_signal.tv_usec / 1000);
-
-	if (difference >= 6 * interval) {
-		write(1, "\n.", 2);
-	} else if (difference >= 3 * interval) {
-		write(1, " .", 2);
-	} else if (difference >= 1 * interval || difference < 0) {
-		write(1, ".", 1);
-	} else {
-		write(1, "!", 1);
-	}
-
-	last_signal.tv_sec = this_signal.tv_sec;
-	last_signal.tv_usec = this_signal.tv_usec;
-}
-
-void receive_dash (int sig)
-{
-	struct timeval this_signal;
-	gettimeofday(&this_signal, NULL);
-
-	int difference = (this_signal.tv_sec * 1000 + this_signal.tv_usec / 1000) -
-		(last_signal.tv_sec * 1000 + last_signal.tv_usec / 1000);
-
-	if (difference >= 6 * interval) {
-		write(1, "\n-", 2);
-	} else if (difference >= 3 * interval) {
-		write(1, " -", 2);
-	} else if (difference >= 1 * interval || difference < 0) {
-		write(1, "-", 1);
-	} else {
-		write(1, "!", 1);
-	}
-
-	last_signal.tv_sec = this_signal.tv_sec;
-	last_signal.tv_usec = this_signal.tv_usec;
-}
+#include "morse_handler.h"
+#include "stopwatch.h"
 
 int main (int argc, char *argv[])
 {
+	int interval, ms; stopwatch last;
+
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s interval\n", argv[0]);
 		return EXIT_FAILURE;
@@ -69,14 +17,21 @@ int main (int argc, char *argv[])
 
 	interval = atoi(argv[1]);
 
-	printf("%d\n", getpid());
-
 	signal(SIGINT, quit);
 	signal(SIGUSR1, receive_dot);
 	signal(SIGUSR2, receive_dash);
 
-	while (keep_going)
-		;
+	while (keep_going) {
+		if (received_dot) {
+			ms = ms_elapsed(&last);
+			printf(".\t%d\n", ms);
+			received_dot = FALSE;
+		} else if (received_dash) {
+			ms = ms_elapsed(&last);
+			printf("-\t%d\n", ms);
+			received_dash = FALSE;
+		}
+	}
 
 	return EXIT_SUCCESS;
 }
