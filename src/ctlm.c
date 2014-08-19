09e9c411f0dbe037f33c5d8d47d2e5d599537060
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-08-19 08:31:12 +0200

    Add command line parsing.

--- src/ctlm.c
+++ src/ctlm.c
@@ -3,10 +3,15 @@
 
 int main (int argc, char *argv[])
 {
+	int interval, test_cases;
+
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
 		return EXIT_FAILURE;
 	}
+
+	interval = atoi(argv[1]);
+	test_cases = atoi(argv[2]);
 
 	return EXIT_SUCCESS;
 }
