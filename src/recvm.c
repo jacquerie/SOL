bfb4d30c032a507d0bc2346aca08dde064fce01c
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-03 19:17:29 +0200

    Use morse buffer abstraction.

--- src/recvm.c
+++ src/recvm.c
@@ -1,21 +1,20 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 #include "constants.h"
-#include "morse.h"
+#include "morse_buffer.h"
 #include "stopwatch.h"
 
 int main (int argc, char *argv[])
 {
-	char buffer[8] = {0}, c;
-	int first_symbol, i = 0, interval, ms, sig;
+	char c;
+	int first, interval, ms, sig;
 	stopwatch last;
 
 	if (argc != 2) {
 		fprintf(stderr, "Usage: %s interval\n", argv[0]);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	interval = atoi(argv[1]);
@@ -27,37 +26,27 @@ int main (int argc, char *argv[])
 	sigaddset(&set, SIGUSR2);
 	sigprocmask(SIG_SETMASK, &set, NULL);
 
-	first_symbol = TRUE;
+	first = TRUE;
 	while (TRUE) {
 		sigwait(&set, &sig);
 		if (sig == SIGINT) {
-			buffer[i] = '\0';
-			write(1, decode(buffer), 1);
-			write(1, "\n", 1);
+			buffer_flush("\n");
 			exit(EXIT_SUCCESS);
 		}
 
 		ms = ms_elapsed(&last);
 		c = (sig == SIGUSR1) ? '.' : '-';
 
-		if (first_symbol || (ms >= interval * BETWEEN_SYMBOLS && ms < interval * BETWEEN_LETTERS)) {
-			buffer[i] = c;
-			i++;
+		if (first || (ms >= interval * BETWEEN_SYMBOLS && ms < interval * BETWEEN_LETTERS)) {
+			buffer_add(c);
 		} else if (ms >= interval * BETWEEN_LETTERS && ms < interval * BETWEEN_WORDS) {
-			buffer[i] = '\0';
-			write(1, decode(buffer), 1);
-			i = 0;
-			buffer[i] = c;
-			i++;
+			buffer_flush(NULL);
+			buffer_add(c);
 		} else if (ms >= interval * BETWEEN_WORDS) {
-			buffer[i] = '\0';
-			write(1, decode(buffer), 1);
-			write(1, " ", 1);
-			i = 0;
-			buffer[i] = c;
-			i++;
+			buffer_flush(" ");
+			buffer_add(c);
 		}
 
-		first_symbol = FALSE;
+		first = FALSE;
 	}
 }
