0f713999514e8253e0c05a9bb56a9a2cae8356f8
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-05 10:45:52 +0200

    Use exit instead of return.

--- src/ctlm.c
+++ src/ctlm.c
@@ -7,11 +7,11 @@ int main (int argc, char *argv[])
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
-		return EXIT_FAILURE;
+		exit(EXIT_FAILURE);
 	}
 
 	interval = atoi(argv[1]);
 	test_cases = atoi(argv[2]);
 
-	return EXIT_SUCCESS;
+	exit(EXIT_SUCCESS);
 }
