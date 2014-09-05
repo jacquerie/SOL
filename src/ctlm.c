85dca0b20d9732b50910a64a971a6f064e7393a3
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-05 12:19:25 +0200

    Use random message library.

--- src/ctlm.c
+++ src/ctlm.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+#include "constants.h"
+#include "random_msg.h"
 
 int main (int argc, char *argv[])
 {
-	int interval, test_cases;
+	char* msg;
+	int i, interval, length, test_cases;
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
@@ -12,6 +17,13 @@ int main (int argc, char *argv[])
 
 	interval = atoi(argv[1]);
 	test_cases = atoi(argv[2]);
+
+	srand(time(NULL));
+	for (i = 0; i < test_cases; i++) {
+		length = random_int(MIN_MSG_LEN, MAX_MSG_LEN);
+		msg = random_msg(length);
+		printf("%s\n", msg);
+	}
 
 	exit(EXIT_SUCCESS);
 }
