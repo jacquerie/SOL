ed128d79533574d8851cadefa1bcfafd6b5d06b5
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-09-08 18:01:23 +0200

    Use experiment abstraction.

--- src/ctlm.c
+++ src/ctlm.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "random_msg.h"
-
-#define MIN_MSG_LEN 16
-#define MAX_MSG_LEN 8192
+#include "experiment.h"
 
 int main (int argc, char *argv[])
 {
-	char* msg;
-	int i, interval, test_cases;
+	int error, mid, min, max;
+	int interval, test_cases;
 
 	if (argc != 3) {
 		fprintf(stderr, "Usage: %s interval test_cases\n", argv[0]);
@@ -19,10 +16,28 @@ int main (int argc, char *argv[])
 	interval = atoi(argv[1]);
 	test_cases = atoi(argv[2]);
 
-	for (i = 0; i < test_cases; i++) {
-		msg = random_msg(MIN_MSG_LEN, MAX_MSG_LEN);
-		printf("%s\n", msg);
-		free(msg);
+	min = 1;
+	max = interval;
+
+	error = TRUE;
+	while (error) {
+		error = experiment(max, test_cases);
+
+		if (error) {
+			min = max;
+			max *= 2;
+		}
+	}
+
+	while (min < max) {
+		mid = min + (max - min)/2;
+		error = experiment(mid, test_cases);
+
+		if (error) {
+			min = mid;
+		} else {
+			max = mid;
+		}
 	}
 
 	exit(EXIT_SUCCESS);
