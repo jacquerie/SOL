89e36fd167d33d4de122ee29cd69dc815652d9fd
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-10-25 19:04:20 +0200

    Add public interface for dotnoise.

--- src/dotnoise.h
+++ src/dotnoise.h
@@ -1,4 +1,15 @@
-#ifndef __DOTNOISE_H_
-#define __DOTNOISE_H_
+#ifndef __DOTNOISE_H
+#define __DOTNOISE_H
+
+typedef struct dotnoiseCompletions {
+	size_t length;
+	char** completions;
+} dotnoiseCompletions;
+
+typedef void(dotnoiseCompletionCallback)(const char*, dotnoiseCompletions*);
+void dotnoiseSetCompletionCallback (dotnoiseCompletionCallback*);
+void dotnoiseAddCompletion (const char*, dotnoiseCompletions*);
+
+char* dotnoise (const char* prompt);
 
 #endif
