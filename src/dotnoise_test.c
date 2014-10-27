9b2251e8fed6c56383d2691ab7773c32543ef680
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2014-10-27 21:41:34 +0100

    Add test for dotnoise.
    Test is a simplified version of example.c from antirez/linenoise.

--- src/dotnoise_test.c
+++ src/dotnoise_test.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "dotnoise.h"
 
+void completion (const char *buffer, dotnoiseCompletions *dc)
+{
+	if (buffer[0] == 'f') {
+		dotnoiseAddCompletion("foo", dc);
+		dotnoiseAddCompletion("foobar", dc);
+	}
+}
+
 int main (void)
 {
+	char *line;
+
+	dotnoiseSetCompletionCallback(completion);
+
+	while ((line = dotnoise("> ")) != NULL) {
+		printf("echo: '%s'\n", line);
+
+		free(line);
+	}
+
 	return 0;
 }
